024eafed3e3a71f37d7e59ebe680c60289dc58e9
jhenaom6 <jhenaom6@eafit.edu.co>
2020-11-19 16:54:55 -0500

    Añadiendo cambios para el envío de mails en ambiente productivo con SENDGRID

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,43 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const sgtransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'tanner.yost@ethereal.email',
-        pass: 'ssAh3ntjy6QMjPWYZj'
+let mailConfig;
+
+if(process.env.NODE_ENV === 'production'){
+
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    
+    mailConfig = sgtransport(options);
+
+} else {
+    //ambiente de servidor de pruebas
+    if(process.env.NODE_ENV === 'staging') {
+
+        log('::::: STAGING ::::::');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+
+        mailConfig = sgtransport(options);
+
+    } else {
+
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_USER,
+                pass: process.env.ETHEREAL_PASS
+            }
+        };
+        
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
